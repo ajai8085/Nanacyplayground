7619bc1343f03c73b41fa02062c75f38e3ffce05
Ajai <ajai.np@gmail.com>
2016-03-09 14:10:05 +1100

    working nancyselfhosted

--- SelfHosting.Nancy/Bootstrapper.cs
+++ SelfHosting.Nancy/Bootstrapper.cs
@@ -1,10 +1,17 @@
 ﻿using Nancy;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.Hosting.Self;
+using Nancy.Responses;
+using Nancy.TinyIoc;
+using Nancy.ViewEngines;
 
 namespace SelfHosting.Nancy
 {
@@ -27,13 +34,68 @@ namespace SelfHosting.Nancy
 
         }
 
-        protected override IRootPathProvider RootPathProvider
+        //protected override IRootPathProvider RootPathProvider
+        //{
+        //    get
+        //    {
+        //        //return new SelfHostingRootPathProvider();
+        //        return new FileSystemRootPathProvider();
+        //    }
+        //}
+
+        protected override NancyInternalConfiguration InternalConfiguration
         {
             get
             {
-                //return new SelfHostingRootPathProvider();
-                return new FileSystemRootPathProvider();
+                return NancyInternalConfiguration.WithOverrides(OnConfigurationBuilder);
             }
+        }
+
+        void OnConfigurationBuilder(NancyInternalConfiguration x)
+        {
+            x.ViewLocationProvider = typeof(ResourceViewLocationProvider);
+        }
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            ResourceViewLocationProvider.RootNamespaces.Add(Assembly.GetAssembly(typeof(Bootstrapper)), "SelfHosting.Nancy.Views");
+        }
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(AddStaticResourcePath("/Content", Assembly.GetAssembly(typeof(Bootstrapper)), "SelfHosting.Nancy.Content"));
+        }
+
+        private static Func<NancyContext, string, Response> AddStaticResourcePath(string requestedPath, Assembly assembly, string namespacePrefix)
+        {
+            return (context, s) =>
+            {
+                var path = context.Request.Path;
+                if (!path.StartsWith(requestedPath))
+                {
+                    return null;
+                }
+
+                string resourcePath;
+                string name;
+
+                var adjustedPath = path.Substring(requestedPath.Length + 1);
+                if (adjustedPath.IndexOf('/') >= 0)
+                {
+                    name = Path.GetFileName(adjustedPath);
+                    resourcePath = namespacePrefix + "." + adjustedPath.Substring(0, adjustedPath.Length - name.Length - 1).Replace('/', '.');
+                }
+                else
+                {
+                    name = adjustedPath;
+                    resourcePath = namespacePrefix;
+                }
+                return new EmbeddedFileResponse(assembly, resourcePath, name);
+            };
         }
     }
 }
--- SelfHosting.Nancy/SelfHostingRootpathProvider.cs
+++ SelfHosting.Nancy/SelfHostingRootpathProvider.cs
@@ -10,16 +10,16 @@ using System.Threading.Tasks;
 namespace SelfHosting.Nancy
 {
     //Not used , use this class if you wish to avoid copy to output for views and content folders 
-    public class SelfHostingRootPathProvider : IRootPathProvider
-    {
-        readonly IRootPathProvider _rootPathProvider;
-        public SelfHostingRootPathProvider()
-        {
-            _rootPathProvider = new FileSystemRootPathProvider();
-        }
-        public string GetRootPath()
-        {
-            return Path.GetFullPath(Path.Combine(_rootPathProvider.GetRootPath(), "..", ".."));
-        }
-    }
+    //public class SelfHostingRootPathProvider : IRootPathProvider
+    //{
+    //    readonly IRootPathProvider _rootPathProvider;
+    //    public SelfHostingRootPathProvider()
+    //    {
+    //        _rootPathProvider = new FileSystemRootPathProvider();
+    //    }
+    //    public string GetRootPath()
+    //    {
+    //        return Path.GetFullPath(Path.Combine(_rootPathProvider.GetRootPath(), "..", ".."));
+    //    }
+    //}
 }
