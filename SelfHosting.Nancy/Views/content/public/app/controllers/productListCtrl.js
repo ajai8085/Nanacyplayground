1c6c8d23ae9e150b5b06d9e331290158d96684f0
Ajai <ajai.np@gmail.com>
2016-03-14 19:59:55 +1100

    fixes

---
+++ SelfHosting.Nancy/Views/content/public/app/controllers/productListCtrl.js
@@ -0,0 +1,71 @@
+﻿(function() {
+    angular.module("productManagement")
+        .controller("ProductListCtrl", ["productResource", productListCtrl]);
+
+    function productListCtrl(productResource) {
+        var vm = this;
+
+        vm.showImage = false;
+        vm.toggleImage=function() {
+            vm.showImage = !vm.showImage;
+        }
+
+
+        productResource.query(function(data) {
+            vm.products = data;
+        });
+
+
+        //vm.products = [
+        //    {
+        //        "productId": 1,
+        //        "productName": "Sample product 1",
+        //        "price": 19.89,
+        //        "cost": 9.0,
+        //        "imageUrl":"http://",
+        //        tags: ["leaf", "tool"]
+        //    },
+        //    {
+        //        "productId": 1,
+        //        "productName": "Sample product 1",
+        //        "price": 19.89,
+        //        "cost": 9.0,
+        //        "imageUrl": "http://",
+        //        tags: ["leaf", "tool"]
+        //    },
+        //    {
+        //        "productId": 1,
+        //        "productName": "Sample product 1",
+        //        "price": 19.89,
+        //        "cost": 9.0,
+        //        "imageUrl": "http://",
+        //        tags: ["leaf", "tool"]
+        //    },
+        //    {
+        //        "productId": 1,
+        //        "productName": "Sample product 1",
+        //        "price": 19.89,
+        //        "cost": 9.0,
+        //        "imageUrl": "http://",
+        //        tags: ["leaf", "tool"]
+        //    },
+        //    {
+        //        "productId": 1,
+        //        "productName": "Sample product 1",
+        //        "price": 19.89,
+        //        "cost": 9.0,
+        //        "imageUrl": "http://",
+        //        tags: ["leaf", "tool"]
+        //    },
+        //    {
+        //        "productId": 1,
+        //        "productName": "Sample product 1",
+        //        "price": 19.89,
+        //        "cost": 9.0,
+        //        tags: ["leaf", "tool"]
+        //    }
+        //];
+    }
+
+
+}());
